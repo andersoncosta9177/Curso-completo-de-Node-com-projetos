9aa69a13e220419bdccd0a5fa48490c43e7d9ad4
andersoncosta9177 <anderson_costa_miguel@hotmail.com>
2022-08-11 17:57:09 -0300

    Fundamentos npm

---
+++ 3_npm/index.js
@@ -0,0 +1,39 @@
+//   Oque é NPM?  
+// npm  -  node package manager 
+
+// É O PRINCIPAL GERNECIADOR DE PACOTES DO NOD JS 
+
+// para iniciar um projeto simples npm init -y 
+
+// instalando pacotes
+
+// npm install <nome do pacote> 
+// npm install lodash 
+//  O pacote ladash verifica a diferença entre arrays 
+
+// Onde encontrar modulos ? 
+// No site npmjs.com 
+
+// CRIANDO SCRIPTS 
+// "scripts":{
+//     "start": 'node index.js'
+// }
+
+
+
+
+// INSTALANDO PACOTES GLOBAIS 
+// um pacote global nao fica salvo na pasta node modules,
+// ele fica salvo no computador do usuario,
+// A vantagem e que podemos acessar em qualquer lugar via
+
+
+
+
+
+// const _ = require("lodash")
+// const a = [1,2,3,4,9]
+// const b = [3,4,5,7,8]
+
+// const diff = _.difference(a,b);
+// console.log(diff)
